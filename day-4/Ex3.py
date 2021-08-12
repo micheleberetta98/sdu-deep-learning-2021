2dae3411ad47a602717b380b0feb85bcae432761
gurtntan <75908146+gurtntan@users.noreply.github.com>
2021-08-12 17:12:16 +0200

    Update Ex3.py

--- day-4/Ex3.py
+++ day-4/Ex3.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Aug 12 15:06:24 2021
-
-@author: Admin
+Deep Learning Summer Course - Exercise 3
 """
 
 import numpy as np
@@ -12,9 +10,7 @@ from tensorflow.keras import layers
 from tensorflow.keras import regularizers
 
 
-
-
-# %% Reload and test old model
+# %% 1. Load your model again, and use model.evaluate to get the accuracy
 
 model = keras.models.load_model('../day-2/model-1-relu-relu-sigm.h5')
 
@@ -30,21 +26,20 @@ def multi_hot_sequences(sequences, dimension):
         results[i, word_indices] = 1.0
     return results
 
-
-# %%
-# train_data has shape (25_000, NUM_WORDS) ==> print(np.shape(train_data))
 train_data = multi_hot_sequences(train_data, dimension=NUM_WORDS)
 test_data = multi_hot_sequences(test_data, dimension=NUM_WORDS)
-
-# %% Load your model again, and use model.evaluate to get the accuracy
-
 
 score = model.evaluate(test_data, test_labels, verbose = 0) 
 
 print('Test loss:', score[0]) 
 print('Test accuracy:', score[1])
 
-# %% Create new model
+#   Test loss: 1.1862590312957764
+#   Test accuracy: 0.85751998424530
+
+# 2. Try adding some regularization, can you increase the accuracy of the model? 
+
+# %% Model 2  - Create and compile model
 
 model2 = keras.Sequential()
 model2.add(keras.layers.Dense(16, activation='relu', input_shape=(NUM_WORDS,), kernel_regularizer=regularizers.l1(1e-5)))
@@ -55,16 +50,16 @@ model2.compile(optimizer='rmsprop',
               loss='binary_crossentropy',
               metrics=['accuracy'])
 
-# %% Train model
+# %% Model 2 - Train model
 history = model2.fit(train_data, train_labels, epochs=5, validation_data=(test_data,test_labels))
 
-# %%
+# %% Model 2 - Evaluate
 score = model2.evaluate(test_data, test_labels, verbose = 0) 
 
 print('Test loss:', score[0]) 
 print('Test accuracy:', score[1])
 
-# %% Create new model w/dropout
+# %% Model 3  - Create and compile model
 
 model3 = keras.Sequential()
 model3.add(keras.layers.Dense(16, activation='relu', input_shape=(NUM_WORDS,), kernel_regularizer=regularizers.l2(1e-5)))
@@ -77,16 +72,16 @@ model3.compile(optimizer='rmsprop',
               loss='binary_crossentropy',
               metrics=['accuracy'])
 
-# %% Train model
+# %% Model 3  - Create and compile model
 history = model3.fit(train_data, train_labels, epochs=1, validation_data=(test_data,test_labels))
 
-# %%
+# %% Model 3 - Evaluate
 score = model3.evaluate(test_data, test_labels, verbose = 0) 
 
 print('Test loss:', score[0]) 
 print('Test accuracy:', score[1])
 
-# %% Create new model4
+# %% Model 4  - Create and compile model
 
 model4= keras.Sequential()
 model4.add(keras.layers.Dense(16, activation='relu', input_shape=(NUM_WORDS,), kernel_regularizer=regularizers.l2(1e-5), activity_regularizer=regularizers.l2(1e-5)))
@@ -101,14 +96,15 @@ model4.compile(optimizer='rmsprop',
               loss='binary_crossentropy',
               metrics=['accuracy'])
 
-# %% Train model
+# %% Model 4 - Create and compile model
 history = model4.fit(train_data, train_labels, epochs=1, validation_data=(test_data,test_labels))
 
-# %%
+# %% Model 4 - Evaluate
 score = model4.evaluate(test_data, test_labels, verbose = 0) 
 
 print('Test loss:', score[0]) 
 print('Test accuracy:', score[1])
+
 
 
 
