5dccde8908e50a24a9ce78ce746a4f610e8daf08
micheleberetta98 <beretta.m98@gmail.com>
2021-08-18 17:43:53 +0200

    little changes to move images

---
+++ final-project/move_images.py
@@ -0,0 +1,40 @@
+import random
+import shutil
+import os
+from pathlib import Path
+
+
+random.seed(0)
+
+source = Path('./images')
+test_dest = Path('./testing')
+val_dest = Path('./validation')
+train_dest = Path('./training')
+
+for d in [test_dest, val_dest, train_dest]:
+    os.mkdir(d)
+    os.mkdir(d / 'normal')
+    os.mkdir(d / 'pneumonia')
+
+
+def copy_random(no_of_files, status, dest):
+    all_files = [x for x in os.listdir(source) if status in x]
+    random_filenames = random.sample(all_files, no_of_files)
+    for filename in random_filenames:
+        shutil.move(source / filename, dest / status / filename)
+
+
+print('Moving 900 files to training... ', end='')
+copy_random(900, "normal", train_dest)
+copy_random(900, "pneumonia", train_dest)
+print('Done')
+
+print('Moving 100 files to testing... ', end='')
+copy_random(100, "normal", test_dest)
+copy_random(100, "pneumonia", test_dest)
+print('Done')
+
+print('Moving 100 files to validation... ', end='')
+copy_random(100, "normal", val_dest)
+copy_random(100, "pneumonia", val_dest)
+print('Done')
--- final-project/task1.py
+++ final-project/task1.py
@@ -2,91 +2,85 @@
 """
 Task 1
 """
+# %%
 
 import numpy as np
 import os
 import random
+from pathlib import Path
 
 import shutil
 
-#%% set directories
-directory = r'C:\Users\Admin\OneDrive\ZHAW\17_DeepLearning\Project\data'
+random.seed(0)
 
-#%% get files
+# %% set directories
+directory = Path('./images')
 
-pneumonia_files = [f for f in os.listdir(directory) if f.endswith('pneumonia.jpg')]
+training_dir = Path('./training')
+testing_dir = Path('./testing')
+validation_dir = Path('./validation')
+
+# %% get files
+
+
+pneumonia_files = [f for f in os.listdir(
+    directory) if f.endswith('pneumonia.jpg')]
 normal_files = [f for f in os.listdir(directory) if f.endswith('normal.jpg')]
 
-#%% assign files
+# %% assign files
 
-list_pneumonia = list(range(1, 1100))
-list_normal= list(range(1, 1100))
+list_pneumonia = list(range(1, 1100 + 1))
+list_normal = list(range(1, 1100 + 1))
 
-train_pneumonia = random.sample(list_pneumonia, 440)
-train_normal = random.sample(list_normal, 440)
+train_pneumonia = random.sample(list_pneumonia, 900)
+train_normal = random.sample(list_normal, 900)
 
 list_pneumonia = [x for x in list_pneumonia if x not in train_pneumonia]
 list_normal = [x for x in list_normal if x not in train_normal]
 
-test_pneumonia = random.sample(list_pneumonia, 55)
-test_normal = random.sample(list_normal, 55)
+test_pneumonia = random.sample(list_pneumonia, 100)
+test_normal = random.sample(list_normal, 100)
 
 list_pneumonia = [x for x in list_pneumonia if x not in test_pneumonia]
 list_normal = [x for x in list_normal if x not in test_normal]
 
-val_pneumonia = random.sample(list_pneumonia, 55)
-val_normal = random.sample(list_normal, 55)
+val_pneumonia = random.sample(list_pneumonia, 100)
+val_normal = random.sample(list_normal, 100)
 
-#%% create file name
+# %% create file name
 pneumonia = "_pneumonia.jpg"
 normal = "_normal.jpg"
 
-train_pneumonia = [str(number) + pneumonia for number in train_pneumonia]
-test_pneumonia = [str(number) + pneumonia for number in test_pneumonia]
-val_pneumonia = [str(number) + pneumonia for number in val_pneumonia]
+train_pneumonia = [directory / (str(number) + pneumonia)
+                   for number in train_pneumonia]
+test_pneumonia = [directory / (str(number) + pneumonia)
+                  for number in test_pneumonia]
+val_pneumonia = [directory / (str(number) + pneumonia)
+                 for number in val_pneumonia]
 
-train_normal = [str(number) + normal for number in train_normal]
-test_normal = [str(number) + normal for number in test_normal]
-val_normal = [str(number) + normal for number in val_normal]
-
+train_normal = [directory / (str(number) + normal) for number in train_normal]
+test_normal = [directory / (str(number) + normal) for number in test_normal]
+val_normal = [directory / (str(number) + normal) for number in val_normal]
 
 
 # %% copy / move files
 
-#shutil.move
+# shutil.move
 
 for f in train_pneumonia:
-    try:
-        shutil.copy(f, r'C:\Users\Admin\OneDrive\ZHAW\17_DeepLearning\Project\Training\pneumonia')
-    except:
-        continue
-    
+    shutil.copy(f, training_dir / 'pneumonia')
+
 for f in train_normal:
-    try:
-        shutil.copy(f, r'C:\Users\Admin\OneDrive\ZHAW\17_DeepLearning\Project\Training\normal')
-    except:
-        continue
-    
+    shutil.copy(f, training_dir / 'normal')
+
 for f in val_pneumonia:
-    try:
-        shutil.copy(f, r'C:\Users\Admin\OneDrive\ZHAW\17_DeepLearning\Project\Validation\pneumonia')
-    except:
-        continue
-    
+    shutil.copy(f, validation_dir / 'pneumonia')
+
 for f in val_normal:
-    try:
-        shutil.copy(f, r'C:\Users\Admin\OneDrive\ZHAW\17_DeepLearning\Project\Validation\normal')
-    except:
-        continue
+    shutil.copy(f, validation_dir / 'normal')
 
 for f in test_pneumonia:
-    try:
-        shutil.copy(f, r'C:\Users\Admin\OneDrive\ZHAW\17_DeepLearning\Project\Testing\pneumonia')
-    except:
-        continue
-    
+    shutil.copy(f, testing_dir / 'pneumonia')
+
 for f in test_normal:
-    try:
-        shutil.copy(f, r'C:\Users\Admin\OneDrive\ZHAW\17_DeepLearning\Project\Testing\normal')
-    except:
-        continue
+    shutil.copy(f, testing_dir / 'normal')
