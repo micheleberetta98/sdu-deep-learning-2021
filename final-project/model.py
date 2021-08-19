2180782c715eb1d38226079989593ece0db64b06
biancolinaa <bianca.crippa@tiscali.it>
2021-08-19 18:23:46 +0200

    Finalizing model.py

--- final-project/model.py
+++ final-project/model.py
@@ -11,13 +11,13 @@ in order to have the highest accuracy possible
 import tensorflow as tf
 import wandb
 from wandb.keras import WandbCallback
-from tensorflow.keras.layers import Dense, Conv2D, Concatenate, AvgPool2D, MaxPooling2D, Flatten, Dropout, Input
+from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout, Input
 from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.regularizers import l2
 import matplotlib.pyplot as plt
 
-from constants import IMG_SIZE, NUM_OF_IMAGES_VAL, NUM_OF_IMAGES_TRAIN, EPOCHS, BATCH_SIZE
+from constants import IMG_SIZE, NUM_OF_IMAGES_VAL, NUM_OF_IMAGES_TRAIN, BATCH_SIZE
 from image_generation import validation_generator, train_generator, test_generator
 from model_hyperparameters import evaluate_model
 
@@ -26,19 +26,22 @@ from model_hyperparameters import evaluate_model
 # The hyperparameters
 kernel_size = (7, 7)
 n_filters = 32
-# input_dropout = 0
 dense_dropout = 0.1
 units = [128, 128, 64]
+l2_k = 0.01
+
+# Kernel size and the number of convolutional layers were chosen as explained in model_hyperparameters.py
+# The other ones were chosen by trial and error.
+# We also noticed that a dropout right after the input wasn't so beneficial as an output in the middle of the
+# model, and that a l1 regularizer (or a l1-l2) was too much aggressive so we went for a l2 regularizer.
+# Moreover, we tried to have multiple branches on the convolutional aprt, but performance didn't seem
+# to be affected a lot positively, so we went for a simpler linear model.
 
 # This is the final model comprising of all of the hyperparameters seen in model_hyperparameters
 # Here we test for dropout and regularization in order to reduce overfitting
 
 inputs = Input(shape=(IMG_SIZE, IMG_SIZE, 1), name='input_layer')
 x = inputs
-
-# Dropout after the input could be beneficial in order to generalize better
-# and reduce overfitting
-# x = Dropout(input_dropout, name='input_dropout')(x)
 
 # We work on images, so a CNN (Convolutional Neural Network) is useful
 # Here we try a couple of convolutional layers with max pooling 
@@ -48,13 +51,15 @@ x = MaxPooling2D((3, 3), name='max_2d_1')(x)
 x = Conv2D(n_filters, kernel_size, activation='relu', name='conv_2d_2')(x)
 x = MaxPooling2D((3, 3), name='max_2d_2')(x)
 
+# Dropout after the convolutional layers could be beneficial in order to generalize better
+# and reduce overfitting on the dense layers
 x = Dropout(dense_dropout, name='dense_dropout')(x)
 x = Flatten(name='flatten_1')(x)
 
 # After the convolutional, a couple of dense layers to learn
-x = Dense(units[0], activation='relu', kernel_regularizer=l2(0.01), name='dense_1')(x)
-x = Dense(units[1], activation='relu', kernel_regularizer=l2(0.01), name='dense_2')(x)
-x = Dense(units[2], activation='relu', kernel_regularizer=l2(0.01), name='dense_3')(x)
+x = Dense(units[0], activation='relu', kernel_regularizer=l2(l2_k), name='dense_1')(x)
+x = Dense(units[1], activation='relu', kernel_regularizer=l2(l2_k), name='dense_2')(x)
+x = Dense(units[2], activation='relu', kernel_regularizer=l2(l2_k), name='dense_3')(x)
 
 # Since we are dealing with a binary problem (pneumonia or normal),
 # the output is given by this 1 neuron with a sigmoid activation function
@@ -81,6 +86,9 @@ early_stopping = EarlyStopping(
     restore_best_weights=True,
 )
 
+# We are using wandb to log the data and to provide graphs, which are visible
+# at https://wandb.ai/micheleberetta98/sdu-deep-learning-final?workspace=user-micheleberetta98
+# (we also saved the images for the best networks)
 wandb.init(project='sdu-deep-learning-final', entity='micheleberetta98', name=model.name)
 wandb_callback = WandbCallback(log_weights=True,
                                 log_evaluation=True,
@@ -101,6 +109,6 @@ loss, acc = evaluate_model(model, test_generator)
 print(f'Loss = {loss}')
 print(f'Acc  = {acc}')
 
-# %%
+# %% Saving the model for future use
 
 model.save(f'model_dropout__reg-l2-0.01_middle01.h5')
