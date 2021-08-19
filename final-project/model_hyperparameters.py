9b941f3198ea9af20e5e7b5b8fe44b4d357d7ce5
biancolinaa <bianca.crippa@tiscali.it>
2021-08-19 13:07:50 +0200

    Renamed file

--- final-project/model_hyperparameters.py
+++ final-project/model_hyperparameters.py
@@ -21,8 +21,12 @@ from image_generation import train_generator, test_generator, validation_generat
 
 def create_model(name='', input_dropout=0, num_of_kernels=32, dense_dropout=0, kernel_size=(5, 5), dense_units=64):
     '''A function with some useful parameters in order to create easily different models'''
+    
     inputs = Input(shape=(IMG_SIZE, IMG_SIZE, 1), name='input_layer')
     x = inputs
+
+    # Dropout after the input could be beneficial in order to generalize better
+    # and reduce overfitting
     x = Dropout(input_dropout, name='input_dropout')(x)
 
     # We work on images, so a CNN (Convolutional Neural Network) is useful
@@ -35,8 +39,6 @@ def create_model(name='', input_dropout=0, num_of_kernels=32, dense_dropout=0, k
     x = Flatten(name='flatten')(x)
 
     # After the convolutional, a couple of dense layers to learn
-    # This number of layers could be cross-val-decided too, as well as the number of units
-    # Also maybe adding some dropout in the middle?
     x = Dense(dense_units, activation='relu', name='dense_1')(x)
     x = Dropout(dense_dropout, name='dense_dropout1')(x)
     x = Dense(dense_units, activation='relu', name='dense_2')(x)
@@ -58,6 +60,7 @@ def create_model(name='', input_dropout=0, num_of_kernels=32, dense_dropout=0, k
 
 
 def evaluate_model(model, val_generator, verbose=0):
+    '''Utility function to evaluate a model on a given validation generator'''
     score = model.evaluate(val_generator, verbose=verbose)
     return score[0], score[1]
 
@@ -75,6 +78,7 @@ early_stopping = EarlyStopping(
 )
 
 def fit_model(model):
+    '''Utility function to fit the model with the correct callbacks (early stopping and wandb)'''
     wandb.init(project='sdu-deep-learning-final', entity='micheleberetta98', name=model.name)
     wandb_callback = WandbCallback(log_weights=True,
                                    log_evaluation=True,
@@ -93,6 +97,7 @@ def fit_model(model):
     return history
 
 def test_model(ks=[], create=lambda x: x):
+    '''Utility functions that goes through all hyperparameters ks and choose the one that minimizes test loss'''
     best_model = None
     best_k = None
     best_loss = float('inf')
@@ -118,6 +123,10 @@ def test_model(ks=[], create=lambda x: x):
 
 # %% Finding the best kernel size
 
+# Here we test different hyperparameters for the convolutional part:
+# - the kernel size
+# - the number of filters
+
 kernel, model_kernel, history_kernel = test_model(
     ks=[(3, 3), (5, 5), (7, 7)],
     create=lambda x: create_model(name=f'model_kernel_{x[0]}x{x[1]}', kernel_size=x))
@@ -130,17 +139,9 @@ num_of_kernels, model_num_kernels, history_num_kernels = test_model(
     create=lambda x: create_model(name=f'{base_name}_num_kernels_{x}', kernel_size=kernel)
 )
 
-# %% Finding the best units for the dense layer
-
-units, model_units, history_units = test_model(
-    ks=[16, 32, 64, 128],
-    create=lambda x: create_model(name=f'model_units_{x}',
-                                  kernel_size=kernel,
-                                  num_of_kernels=num_of_kernels,
-                                  dense_units=x)
-)
-
 # %% Putting together and training the model
+
+# This is the final model comprising of all of the hyperparameters seen before
 
 model = create_model(name='model_no_reg',
                      num_of_kernels=num_of_kernels,
@@ -152,18 +153,20 @@ loss, acc = evaluate_model(model, validation_generator)
 print(f'Loss = {loss}')
 print(f'Acc  = {acc}')
 
-# %% Adding dropout
+# # %% Adding dropout
 
-input_drop, _, _ = test_model(
-    ks=[0.1, 0.2, 0.4, 0.5, 0.6],
-    create=lambda x: create_model(
-        name=f'model_in_drop_{x}',
-        input_dropout=x,
-        num_of_kernels=num_of_kernels,
-        kernel_size=kernel,
-        dense_units=units)
-)
+# # Here we add dropout and regularization in order to prevent overfitting
 
-# %% Save the model
+# input_drop, _, _ = test_model(
+#     ks=[0.1, 0.2, 0.4, 0.5, 0.6],
+#     create=lambda x: create_model(
+#         name=f'model_in_drop_{x}',
+#         input_dropout=x,
+#         num_of_kernels=num_of_kernels,
+#         kernel_size=kernel,
+#         dense_units=units)
+# )
 
-model.save('model.h5')
+# # %% Save the model
+
+# model.save('model.h5')
