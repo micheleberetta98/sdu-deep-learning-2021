d657af4d320a9f9c9a28b4018b793ec22cdc3733
micheleberetta98 <beretta.m98@gmail.com>
2021-08-19 10:04:23 +0200

    First iteration of model creation and cross-validation

--- final-project/image_generation.py
+++ final-project/image_generation.py
@@ -1,33 +1,27 @@
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D
-from tensorflow.keras.layers import MaxPooling2D
-from tensorflow.keras.layers import Flatten
-from tensorflow.keras.layers import Dense
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-IMG_SIZE = 256
-BATCH_SIZE = 45
-NUM_OF_IMAGES_TRAIN = 1800
-NUM_OF_IMAGES_VAL = 200
+from constants import IMG_SIZE, BATCH_SIZE, NUM_OF_IMAGES_TRAIN, NUM_OF_IMAGES_VAL
 
-model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape = (IMG_SIZE, IMG_SIZE, 1), activation = 'relu')) # this helps the tensor of output
-model.add(MaxPooling2D(pool_size = (2, 2))) # i used pooling since we need max value pixel for the ROI
-model.add(Flatten()) #then flatten
-model.add(Dense(128, activation = 'relu'))
-model.add(Dense(1, activation = 'sigmoid'))
+# model = Sequential()
+# model.add(Conv2D(32, (3, 3), input_shape=(IMG_SIZE, IMG_SIZE, 1),
+#           activation='relu'))  # this helps the tensor of output
+# # i used pooling since we need max value pixel for the ROI
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Flatten())  # then flatten
+# model.add(Dense(128, activation='relu'))
+# model.add(Dense(1, activation='sigmoid'))
 
-model.compile(optimizer = 'adam',
-                   loss = 'binary_crossentropy',
-                   metrics = ['accuracy'])
+# model.compile(optimizer='adam',
+#               loss='binary_crossentropy',
+#               metrics=['accuracy'])
 
 # we start with the image data generator
 training_datagenerator = ImageDataGenerator(rescale=1/255,
-                                   rotation_range=10,
-                                   zoom_range=0.2,
-                                   shear_range=0.5,
-                                   width_shift_range=0.1,
-                                   height_shift_range=0.1)
+                                            rotation_range=10,
+                                            zoom_range=0.2,
+                                            shear_range=0.5,
+                                            width_shift_range=0.1,
+                                            height_shift_range=0.1)
 # i used the rescale parameter for normalization
 # so it basically transforms the pixels from [0…255] to [0…1]
 
@@ -37,32 +31,20 @@ testing_datagenerator = ImageDataGenerator(rescale=1/255)
 train_generator = training_datagenerator.flow_from_directory(
     './training',
     color_mode='grayscale',
-    target_size = (IMG_SIZE,IMG_SIZE), # refers to the size width and height
-    batch_size = BATCH_SIZE,
-    class_mode = 'binary')
+    target_size=(IMG_SIZE, IMG_SIZE),  # refers to the size width and height
+    batch_size=BATCH_SIZE,
+    class_mode='binary')
 
 validation_generator = testing_datagenerator.flow_from_directory(
     './validation',
     color_mode='grayscale',
-    target_size = (IMG_SIZE, IMG_SIZE),
-    batch_size = BATCH_SIZE,
-    class_mode = 'binary')
-
-# training the model
-history = model.fit(train_generator,
-                    steps_per_epoch = (NUM_OF_IMAGES_TRAIN // BATCH_SIZE),
-                    validation_steps=(NUM_OF_IMAGES_VAL // BATCH_SIZE),
-                    epochs = 10,
-                    use_multiprocessing=True,
-                    workers=8,
-                    validation_data = validation_generator)
-
-#after training the model, we introduce the unseen data from the testing
-#by loading the testing dataset
+    target_size=(IMG_SIZE, IMG_SIZE),
+    batch_size=BATCH_SIZE,
+    class_mode='binary')
 
 test_generator = testing_datagenerator.flow_from_directory(
     './testing',
     color_mode='grayscale',
-    target_size = (IMG_SIZE, IMG_SIZE),
-    batch_size = BATCH_SIZE,
-    class_mode = 'binary')
+    target_size=(IMG_SIZE, IMG_SIZE),
+    batch_size=BATCH_SIZE,
+    class_mode='binary')
---
+++ final-project/model.py
@@ -0,0 +1,107 @@
+# %% Imports
+
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout, Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import EarlyStopping
+
+from constants import IMG_SIZE, NUM_OF_IMAGES_TRAIN, NUM_OF_IMAGES_VAL, BATCH_SIZE, EPOCHS
+from image_generation import train_generator, test_generator, validation_generator
+
+# %% Function utilities for creating and validating models
+
+
+def create_model(name='', input_dropout=0, dense_dropout=0, kernel_size=(5, 5), dense_units=64):
+    '''A function with some useful parameters in order to create easily different models'''
+    inputs = Input(shape=(IMG_SIZE, IMG_SIZE, 1), name='input_layer')
+    x = inputs
+    x = Dropout(input_dropout, name='input_dropout')(x)
+
+    # We work on images, so a CNN (Convolutional Neural Network) is useful
+    # Here we try a couple of convolutional layers with max pooling
+    x = Conv2D(32, kernel_size, activation='relu', name='conv_2d_1')(x)
+    x = MaxPooling2D((3, 3), name='max_2d_1')(x)
+
+    # Maybe one more, and we can also cross-val-decide how much convolutional layers we have
+    x = Conv2D(32, kernel_size, activation='relu', name='conv_2d_2')(x)
+    x = MaxPooling2D((3, 3), name='max_2d_2')(x)
+
+    x = Flatten(name='flatten')(x)
+
+    # After the convolutional, a couple of dense layers to learn
+    # This number of layers could be cross-val-decided too, as well as the number of units
+    # Also maybe adding some dropout in the middle?
+    x = Dense(dense_units, activation='relu', name='dense_1')(x)
+    x = Dropout(dense_dropout, name='dense_dropout1')(x)
+    x = Dense(dense_units, activation='relu', name='dense_2')(x)
+    x = Dropout(dense_dropout, name='dense_dropout2')(x)
+    x = Dense(dense_units, activation='relu', name='dense_3')(x)
+
+    # Since we are dealing with a binary problem (pneumonia or normal),
+    # the output is given by this 1 neuron with a sigmoid activation function
+    output = Dense(1, activation='sigmoid', name='dense_output')(x)
+
+    model = Model(inputs=inputs, outputs=output, name=name)
+
+    model.compile(optimizer='adam',
+                  # Binary crossentropy as a loss function is ideal for a binary problem
+                  loss='binary_crossentropy',
+                  # We are interested in the network accuracy (and also the loss)
+                  metrics=['accuracy'])
+    return model
+
+
+def evaluate_model(model, val_generator, verbose=0):
+    score = model.evaluate(val_generator, verbose=verbose)
+    return score[0], score[1]
+
+
+def test_model(ks=[], create=lambda x: x):
+    best_model = None
+    best_k = None
+    best_loss = float('inf')
+    best_history = None
+
+    # We calculate the steps per epoch so that we use all the images
+    # we have in the folders
+    steps_per_epoch = NUM_OF_IMAGES_TRAIN // BATCH_SIZE
+    val_steps = NUM_OF_IMAGES_VAL // BATCH_SIZE
+
+    # Early stopping can be useful in order to prevent excessive training
+    # and stopping at the best model if loss doesn't get better
+    early_stopping = EarlyStopping(
+        patience=3,
+        monitor="val_loss",
+        restore_best_weights=True,
+    )
+
+    # Here we test against all the hyperparameters in ks
+    # and keep only the model that with the best val_loss
+    # for a specific hyperparameter
+    for k in ks:
+        print(f'Model with {k} ... ', end='')
+        model = create(k)
+        history = model.fit(train_generator,
+                            steps_per_epoch=steps_per_epoch,
+                            validation_steps=val_steps,
+                            validation_data=val_generator,
+                            epochs=EPOCHS,
+                            use_multiprocessing=True,
+                            workers=8,
+                            calbacks=[early_stopping])
+
+        loss, acc = evaluate_model(model, val_generator)
+        print(f"Loss = {loss}")
+        print(f"Acc  = {acc}")
+        if loss < best_loss:
+            best_k = k
+            best_loss = loss
+            best_model = model
+            best_history = history
+
+    return best_k, best_model, best_history
+
+
+kernel, model_kernel, history_kernel = test_model(
+    ks=[(3, 3), (5, 5), (7, 7), (10, 10), (15, 15), (20, 20)],
+    create=lambda k: create_model(name=f'model_kernel_{k}', kernel_size=k))
