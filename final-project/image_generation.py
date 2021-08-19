5f4ab286d83c699e69d487adca31c07971f7bbe5
biancolinaa <bianca.crippa@tiscali.it>
2021-08-19 09:50:47 +0200

    Minor refactor for image generation

--- final-project/image_generation.py
+++ final-project/image_generation.py
@@ -5,11 +5,13 @@ from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-img_dims = 500   #we can still change this
-batch_size = 32
+IMG_SIZE = 256
+BATCH_SIZE = 45
+NUM_OF_IMAGES_TRAIN = 1800
+NUM_OF_IMAGES_VAL = 200
 
 model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape = (img_dims, img_dims, 3), activation = 'relu')) # this helps the tensor of output
+model.add(Conv2D(32, (3, 3), input_shape = (IMG_SIZE, IMG_SIZE, 1), activation = 'relu')) # this helps the tensor of output
 model.add(MaxPooling2D(pool_size = (2, 2))) # i used pooling since we need max value pixel for the ROI
 model.add(Flatten()) #then flatten
 model.add(Dense(128, activation = 'relu'))
@@ -26,41 +28,41 @@ training_datagenerator = ImageDataGenerator(rescale=1/255,
                                    shear_range=0.5,
                                    width_shift_range=0.1,
                                    height_shift_range=0.1)
-# i used the rescale parameter for grayscale normalization
+# i used the rescale parameter for normalization
 # so it basically transforms the pixels from [0…255] to [0…1]
 
 testing_datagenerator = ImageDataGenerator(rescale=1/255)
 
 # as stated, using the .flow_from_directory makes it work with the directory in task 1
 train_generator = training_datagenerator.flow_from_directory(
-    '/Users/mac/PycharmProjects/pythonProject/data/training',
-    target_size = (256,256), # refers to the size width and height
-    batch_size = 32,
+    './training',
+    color_mode='grayscale',
+    target_size = (IMG_SIZE,IMG_SIZE), # refers to the size width and height
+    batch_size = BATCH_SIZE,
     class_mode = 'binary')
 
 validation_generator = testing_datagenerator.flow_from_directory(
-    '/Users/mac/PycharmProjects/pythonProject/data/validation',
-    target_size = (256, 256),
-    batch_size = 32,
+    './validation',
+    color_mode='grayscale',
+    target_size = (IMG_SIZE, IMG_SIZE),
+    batch_size = BATCH_SIZE,
     class_mode = 'binary')
 
 # training the model
-history = model.fit_generator(train_generator,
-                    steps_per_epoch = 10,
+history = model.fit(train_generator,
+                    steps_per_epoch = (NUM_OF_IMAGES_TRAIN // BATCH_SIZE),
+                    validation_steps=(NUM_OF_IMAGES_VAL // BATCH_SIZE),
                     epochs = 10,
+                    use_multiprocessing=True,
+                    workers=8,
                     validation_data = validation_generator)
 
 #after training the model, we introduce the unseen data from the testing
 #by loading the testing dataset
 
 test_generator = testing_datagenerator.flow_from_directory(
-    '/Users/mac/PycharmProjects/pythonProject/data/testing',
-    target_size = (256, 256),
-    batch_size = 32,
+    './testing',
+    color_mode='grayscale',
+    target_size = (IMG_SIZE, IMG_SIZE),
+    batch_size = BATCH_SIZE,
     class_mode = 'binary')
-
-eval_result = model.evaluate_generator(test_generator, 624)
-
-# we can now see the accuracy of the model by running this
-print('loss rate at evaluation data :', eval_result[0])
-print('accuracy rate at evaluation data :', eval_result[1])
