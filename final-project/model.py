6917bf0b17654ea8b1628a825808d9eacbe0aa70
biancolinaa <bianca.crippa@tiscali.it>
2021-08-19 11:57:39 +0200

    Added some basic feature selection

--- final-project/model.py
+++ final-project/model.py
@@ -7,9 +7,11 @@
 # %% Imports
 
 import tensorflow as tf
+import wandb
 from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout, Input
 from tensorflow.keras.models import Model
 from tensorflow.keras.callbacks import EarlyStopping
+from wandb.keras import WandbCallback
 
 from constants import IMG_SIZE, NUM_OF_IMAGES_TRAIN, NUM_OF_IMAGES_VAL, BATCH_SIZE, EPOCHS
 from image_generation import train_generator, test_generator, validation_generator
@@ -17,7 +19,7 @@ from image_generation import train_generator, test_generator, validation_generat
 # %% Function utilities for creating and validating models
 
 
-def create_model(name='', input_dropout=0, dense_dropout=0, kernel_size=(5, 5), dense_units=64):
+def create_model(name='', input_dropout=0, num_of_kernels=32, dense_dropout=0, kernel_size=(5, 5), dense_units=64):
     '''A function with some useful parameters in order to create easily different models'''
     inputs = Input(shape=(IMG_SIZE, IMG_SIZE, 1), name='input_layer')
     x = inputs
@@ -25,11 +27,9 @@ def create_model(name='', input_dropout=0, dense_dropout=0, kernel_size=(5, 5), 
 
     # We work on images, so a CNN (Convolutional Neural Network) is useful
     # Here we try a couple of convolutional layers with max pooling
-    x = Conv2D(32, kernel_size, activation='relu', name='conv_2d_1')(x)
+    x = Conv2D(num_of_kernels, kernel_size, activation='relu', name='conv_2d_1')(x)
     x = MaxPooling2D((3, 3), name='max_2d_1')(x)
-
-    # Maybe one more, and we can also cross-val-decide how much convolutional layers we have
-    x = Conv2D(32, kernel_size, activation='relu', name='conv_2d_2')(x)
+    x = Conv2D(num_of_kernels, kernel_size, activation='relu', name='conv_2d_2')(x)
     x = MaxPooling2D((3, 3), name='max_2d_2')(x)
 
     x = Flatten(name='flatten')(x)
@@ -61,6 +61,36 @@ def evaluate_model(model, val_generator, verbose=0):
     score = model.evaluate(val_generator, verbose=verbose)
     return score[0], score[1]
 
+# We calculate the steps per epoch so that we use all the images
+# we have in the folders
+steps_per_epoch = NUM_OF_IMAGES_TRAIN // BATCH_SIZE
+val_steps = NUM_OF_IMAGES_VAL // BATCH_SIZE
+
+# Early stopping can be useful in order to prevent excessive training
+# and stopping at the best model if loss doesn't get better
+early_stopping = EarlyStopping(
+    patience=3,
+    monitor="val_loss",
+    restore_best_weights=True,
+)
+
+def fit_model(model):
+    wandb.init(project='sdu-deep-learning-final', entity='micheleberetta98', name=model.name)
+    wandb_callback = WandbCallback(log_weights=True,
+                                   log_evaluation=True,
+                                   save_model=True,
+                                   validation_steps=val_steps)
+    history = model.fit(train_generator,
+                        steps_per_epoch=steps_per_epoch,
+                        validation_steps=val_steps,
+                        validation_data=validation_generator,
+                        epochs=EPOCHS,
+                        use_multiprocessing=True,
+                        workers=8,
+                        verbose=2,
+                        callbacks=[early_stopping, wandb_callback])
+
+    return history
 
 def test_model(ks=[], create=lambda x: x):
     best_model = None
@@ -68,35 +98,14 @@ def test_model(ks=[], create=lambda x: x):
     best_loss = float('inf')
     best_history = None
 
-    # We calculate the steps per epoch so that we use all the images
-    # we have in the folders
-    steps_per_epoch = NUM_OF_IMAGES_TRAIN // BATCH_SIZE
-    val_steps = NUM_OF_IMAGES_VAL // BATCH_SIZE
-
-    # Early stopping can be useful in order to prevent excessive training
-    # and stopping at the best model if loss doesn't get better
-    early_stopping = EarlyStopping(
-        patience=3,
-        monitor="val_loss",
-        restore_best_weights=True,
-    )
-
     # Here we test against all the hyperparameters in ks
     # and keep only the model that with the best val_loss
     # for a specific hyperparameter
     for k in ks:
         print(f'Model with {k} ... ', end='')
         model = create(k)
-        history = model.fit(train_generator,
-                            steps_per_epoch=steps_per_epoch,
-                            validation_steps=val_steps,
-                            validation_data=val_generator,
-                            epochs=EPOCHS,
-                            use_multiprocessing=True,
-                            workers=8,
-                            calbacks=[early_stopping])
-
-        loss, acc = evaluate_model(model, val_generator)
+        history = fit_model(model)
+        loss, acc = evaluate_model(model, test_generator)
         print(f"Loss = {loss}")
         print(f"Acc  = {acc}")
         if loss < best_loss:
@@ -107,7 +116,54 @@ def test_model(ks=[], create=lambda x: x):
 
     return best_k, best_model, best_history
 
+# %% Finding the best kernel size
 
 kernel, model_kernel, history_kernel = test_model(
-    ks=[(3, 3), (5, 5), (7, 7), (10, 10), (15, 15), (20, 20)],
-    create=lambda k: create_model(name=f'model_kernel_{k}', kernel_size=k))
+    ks=[(3, 3), (5, 5), (7, 7)],
+    create=lambda x: create_model(name=f'model_kernel_{x[0]}x{x[1]}', kernel_size=x))
+
+# %% Finding the best number of kernels
+
+base_name = f'model_k{kernel[0]}x{kernel[1]}'
+num_of_kernels, model_num_kernels, history_num_kernels = test_model(
+    ks=[8, 16, 32],
+    create=lambda x: create_model(name=f'{base_name}_num_kernels_{x}', kernel_size=kernel)
+)
+
+# %% Finding the best units for the dense layer
+
+units, model_units, history_units = test_model(
+    ks=[16, 32, 64, 128],
+    create=lambda x: create_model(name=f'model_units_{x}',
+                                  kernel_size=kernel,
+                                  num_of_kernels=num_of_kernels,
+                                  dense_units=x)
+)
+
+# %% Putting together and training the model
+
+model = create_model(name='model_no_reg',
+                     num_of_kernels=num_of_kernels,
+                     kernel_size=kernel,
+                     dense_units=units)
+
+history = fit_model(model)
+loss, acc = evaluate_model(model, validation_generator)
+print(f'Loss = {loss}')
+print(f'Acc  = {acc}')
+
+# %% Adding dropout
+
+input_drop, _, _ = test_model(
+    ks=[0.1, 0.2, 0.4, 0.5, 0.6],
+    create=lambda x: create_model(
+        name=f'model_in_drop_{x}',
+        input_dropout=x,
+        num_of_kernels=num_of_kernels,
+        kernel_size=kernel,
+        dense_units=units)
+)
+
+# %% Save the model
+
+model.save('model.h5')
